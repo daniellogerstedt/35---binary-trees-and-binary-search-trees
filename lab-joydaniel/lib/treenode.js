b43176b3ced9034c17eceaeb1fe3085359736f26
daniellogerstedt <daniel.logerstedt@gmail.com>
2018-03-07 14:59:32 -0800

    added documentation

--- lab-joydaniel/lib/treenode.js
+++ lab-joydaniel/lib/treenode.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = class TreeNode {
+module.exports = class TreeNode { // Big O: O(1) constructor.
   constructor(value, left = null, right = null) {
     this.value = value;
     this.left = left;
